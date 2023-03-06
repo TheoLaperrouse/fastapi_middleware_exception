8015011a58fc6f92b27ac2f9ba3b909ab022bea8
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-03-06 15:51:28 +0100

    Correct Middleware

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-
-class SizeLimitException(Exception):
-    def __init__(self, message: str):
-        self.message = message
-
-
-class SizeLimitMiddleware:
-    def __init__(self, app: FastAPI, max_size: int):
-        self.app = app
-        self.max_size = max_size
-
-    async def __call__(self, request: Request, response: Response):
-        content_length = request.headers.get('content-length')
-        if content_length and int(content_length) > self.max_size:
-            raise SizeLimitException(
-                f"La taille de la requête dépasse la limite autorisée : {self.max_size} octets.")
-        else:
-            return await self.app(request, response)
-
-
 app = FastAPI()
+
+
+async def set_body(request: Request, body: bytes):
+    async def receive():
+        return {"type": "http.request", "body": body}
+    request._receive = receive
+
+
+async def get_body(request: Request) -> bytes:
+    body = await request.body()
+    await set_body(request, body)
+    return body
 
 
 class Item(BaseModel):
@@ -35,8 +27,11 @@ async def create_item(item: Item):
     return item
 
 
-@app.exception_handler(SizeLimitException)
-async def size_limit_exception_handler(request: Request, exc: SizeLimitException):
-    return JSONResponse(status_code=418, content={"message": exc.message})
-
-app.add_middleware(SizeLimitMiddleware, max_size=1024)
+@app.middleware("http")
+async def add_middleware_here(request: Request, call_next):
+    content_length = await get_body(request)
+    if content_length and len(content_length) > 1024:
+        return JSONResponse(status_code=413, content='Le content est trop long')
+    else:
+        response = await call_next(request)
+        return response
